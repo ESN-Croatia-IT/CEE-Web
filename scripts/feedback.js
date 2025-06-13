2d62a487f92efb52136295a91845e099e228df84
Luka Šulc <lukasulc0@gmail.com>
2025-06-13 10:28:21 +0200

    Update access token for Userback widget

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,5 +1,5 @@
 window.Userback = window.Userback || {access_token: null};
-Userback.access_token = "H6orrQUIqzDTkifGwabKK1fE3fYYy852495";
+Userback.access_token = "A-UbjQXHKPuEXoucjF0XJLylMHk";
 (function(d) {
   var s = d.createElement('script');s.async = true;s.src = 'https://static.userback.io/widget/v1.js';(d.head || d.body).appendChild(s);
 })(document);
