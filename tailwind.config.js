c1e1cf10c3f24b535714eedb7fc98e581ff50457
Marin Prusac <prusac.marin@gmail.com>
2025-10-21 18:45:55 +0200

    changes for the winter cee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {
-      colors: {
-        ESNblue: '#00aeef',  // Custom named color
-        ESNpink: '#ec008c',   
-        ESNorange: '#f47b20',
-        ESNdarkblue: '#2e3192',
-        ESNdarkblue1000: '#2e3192',
-        ESNdarkblue500: '#8183DE',
-        ESNdarkblue100: '#DCDCFA',
-        ESNgreen: '#7ac143'
-      }
-    },
-  },
-  plugins: [],
+  content: ["./*.html", "./components/*.html"],
 }
 
