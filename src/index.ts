a20143fae952e0f57af78e53b2879f55b38ca283
Marin Prusac <prusac.marin@gmail.com>
2025-12-01 21:39:18 +0100

    migrated all the content to appear as it did before

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import path from 'path';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../dist/static")));
+app.set("views", path.join(__dirname, "../dist/views"));
+
+
+
 app.get('/', (_req, res) => {
-  res.send('It works');
+  res.render('homepage', {});
+});
+
+app.get('/about', (_req, res) => {
+  res.render('about', {});
+});
+
+app.get('/faq', (_req, res) => {
+  res.render('faq', {});
 });
 
 app.listen(PORT, () => {
